e3ddf702a3f5b8d2846cc523effa4e5bcda90872
flolu <loflude@gmail.com>
2021-04-01 11:02:18 +0200

    feat: add sass support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../styles/globals.sass'
 
 import {AppProps} from 'next/app'
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.sass'
 
 export default function Home(): JSX.Element {
   return (
